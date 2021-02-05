77fa2e25b3a8f248c834062c76a81b88a5d36da4
jayvynl <zhiwenlin1116@gmail.com>
2021-02-06 00:13:05 +0800

    fix(i18n): fix the problem of translation to chinese, refer to http://michaelyou.github.io/2015/05/22/Django%E5%9B%BD%E9%99%85%E5%8C%96%E9%81%87%E5%88%B0%E7%9A%84%E7%A5%9E%E5%9D%91/

--- blog/article/models.py
+++ blog/article/models.py
@@ -1,8 +1,8 @@
 from django.db import models
+from django.utils.html import strip_tags
 from django.utils.translation import gettext_lazy as _
 from markdownx.models import MarkdownxField
 from markdownx.utils import markdownify
-from django.utils.html import strip_tags
 
 
 class Article(models.Model):
